7f15e3e0dcbbb231e68738814e0235e470303884
Derek Bringewatt <33988442+dbbring@users.noreply.github.com>
2021-04-20 07:02:29 -0600

    clean up config json

--- hookers/index.py
+++ hookers/index.py
@@ -11,21 +11,9 @@ load_dotenv()
 # discord_api = DiscordAPIClient()
 # links = discord_api.get_existing_links()
 
-with open(os.path.dirname(__file__) + '/config.json') as f:
-    config = json.load(f)
+with open(os.path.dirname(__file__) + '/feeds.json') as f:
+    feeds = json.load(f)
 
-# ============= TODO ================================
-# get a list of links to see if we already posted it
-# only need one list then reference that depends on the
-# channel, maybe setup a dict<channel, list> ? globals not thread safe
-# list needs to be a set and use the in
-
-# use enum and every so often insert banner ads for supporter
-# or other companies / only in free chans, maybe divide total feeds
-# to see how many times we need to show it
-
-# break config into seperate readers then send each off with its own process
-
-for feed in config['feeds']:
+for feed in feeds['feeds']:
     feeditem = FeedItem(feed)
     feeditem.get_feed().save()
